793f5e9050657e066ef0d4ca9f974c72b8284104
Kenta Murata <mrkn@mrkn.jp>
2020-12-03 09:48:41 +0900

    Add animation example

---
+++ example/animation.rb
@@ -0,0 +1,34 @@
+require "unicode_plot"
+require "stringio"
+
+N = 1000
+M = 50
+
+out = StringIO.new
+def out.tty?; true; end
+
+shift = 0
+continue = true
+Signal.trap(:INT) { continue = false }
+
+while continue
+  out.rewind
+
+  xs = 0...N
+  ys = xs.map {|x| Math.sin(2*Math::PI*(x + shift) / N) }
+  UnicodePlot.lineplot(xs, ys, width: 60, height: 15).render(out)
+
+  lines = out.string.lines
+  lines.each do |line|
+    $stdout.print "\r#{line}"
+  end
+  $stdout.flush
+
+  if continue
+    n = lines.count
+    $stdout.print "\e[#{n}F"
+    shift = (shift + M) % N
+  end
+
+  sleep 0.2
+end
