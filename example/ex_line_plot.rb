f725c88a4827c2fcd5bff63128e86817e810fbde
Ben Bowers <nanobowers@gmail.com>
2020-12-29 08:55:06 -0500

    adding comment

--- example/ex_line_plot.rb
+++ example/ex_line_plot.rb
@@ -1,6 +1,7 @@
 #!/bin/env ruby
 require_relative "../lib/unicode_plot"
 
+# example of line plots using different renderers
 UnicodePlot.lineplot([1, 2, 7], [9, -6, 8], title: "Default Lineplot").render
 UnicodePlot.lineplot([1, 2, 7], [9, -6, 8], title: "Ascii Lineplot", canvas: :ascii).render
 UnicodePlot.lineplot([1, 2, 7], [9, -6, 8], title: "Dot Lineplot", canvas: :dot).render
