98404d633aaa2663dfce6853c9cc77237c748444
Kenta Murata <mrkn@mrkn.jp>
2021-02-08 17:32:39 +0900

    Add lineplot document

--- lib/unicode_plot/lineplot.rb
+++ lib/unicode_plot/lineplot.rb
@@ -4,6 +4,32 @@ module UnicodePlot
   class Lineplot < GridPlot
   end
 
+  # @overload lineplot([x], y, name: "", canvas: :braille, title: "", xlabel: "", ylabel: "", labels: true, border: :solid, margin: Plot::DEFAULT_MARGIN, padding: Plot::DEFAULT_PADDING, color: :auto, width: Plot::DEFAULT_WIDTH, height: GridPlot::DEFAULT_HEIGHT, xlim: [0, 0], ylim: [0, 0], canvas: :braille, grid: true)
+  #
+  #   Draws a path through the given points on a new canvas.
+  #
+  #   The first (optional) array `x` should contain the horizontal positions for all the points along the path.
+  #   The second array `y` should then contain the corresponding vertical positions respectively.
+  #   This means that the two vectors must be of the same length and ordering.
+  #
+  #   @param x [Array<Numeric>] Optional. The horizontal position for each point. If omitted, the axes of `y` will be used as `x`.
+  #   @param y [Array<Numeric>] The vertical position for each point.
+  #   @param name [String] Annotation of the current drawing to be displayed on the right.
+  #   @param title
+  #   @param xlabel
+  #   @param ylabel
+  #   @param labels
+  #   @param border
+  #   @param margin
+  #   @param padding
+  #   @param color
+  #   @param width
+  #   @param height
+  #   @param xlim
+  #   @param ylim
+  #   @param canvas [Symbol] The type of canvas that should be used for drawing.
+  #   @param grid [true,false] If `true`, draws grid-lines at the origin.
+  #   @return [Lineplot] A plot object.
   module_function def lineplot(*args,
                                canvas: :braille,
                                color: :auto,
@@ -41,6 +67,16 @@ module UnicodePlot
     end
   end
 
+  # @overload lineplot!(plot, [x], y, name: "", color: :auto)
+  #
+  #   Draws a path through the given points on the given canvas.
+  #
+  #   @param plot [Lineplot] The plot object.
+  #   @param x [Array<Numeric>] Optional. The horizontal position for each point. If omitted, the axes of `y` will be used as `x`.
+  #   @param y [Array<Numeric>] The vertical position for each point.
+  #   @param name [String] Annotation of the current drawing to be displayed on the right.
+  #   @param color
+  #   @return [Lineplot] The plot object same as the `plot` parameter.
   module_function def lineplot!(plot,
                                 *args,
                                 color: :auto,
